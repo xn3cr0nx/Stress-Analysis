ea1fcf927101c6103c59d06008a5cdfe31e11528
Loris Rossi <lorisrossirn@gmail.com>
2017-04-03 11:43:58 +0200

    Rete neurale con 4 feature

--- Code/Neural Network/full_code.py
+++ Code/Neural Network/full_code.py
@@ -11,14 +11,16 @@ def vectorized_result(j):
     e[int(j)-1] = 1.0
     return e
 
-data = np.genfromtxt ('../Dataset/shuffleData.csv', delimiter=',')
+data = np.genfromtxt ('../Dataset/dataset-not-scaled.csv', delimiter=',')
 
 # TODO: shuffle di 'data', cosi ogni volta training e test sono diversi
 
 n_data = len(data[:,0])
 HR = data[:,0]
 GSR = data[:,1]
-labels = data[:,3]
+RR = data[:,2]
+ST = data[:,3]
+labels = data[:,5]
 
 # Il vettore di input deve essere 2x1.
 # Il vettore di output deve essere 3x1 per il training, 1x1 per il test
@@ -26,7 +28,7 @@ labels = data[:,3]
 #training_inputs contiene per ogni entry le due features messe in colonna
 #training_results e un vettore con 1.0 nell' elemento j corrispondente al valore j del valore passato
 
-training_inputs = [np.reshape(x, (2,1)) for x in zip(HR, GSR)]
+training_inputs = [np.reshape(x, (4,1)) for x in zip(HR, GSR, RR, ST)]
 training_results = [vectorized_result(y) for y in labels]
 training_data = zip(training_inputs, training_results)[:65000]
 test_data = zip(training_inputs, labels)[65000:]
@@ -36,21 +38,20 @@ test_data = zip(training_inputs, labels)[65000:]
 # migliori mini batch size: 70, 20
 # migliori nodi hidden layer1: 350, 200
 epochs = 37
-mini_batch_size = 70
+mini_batch_size = 20
 learning_rate = 1.2
-filename = "provaNoLog.csv"
-with open(filename, 'w') as log:
-    log.write("ACCURACY\n")
+# filename = "provaNoLog.csv"
+# with open(filename, 'w') as log:
+#     log.write("ACCURACY\n")
 
 
 for i in xrange(12):
-    
+
     #print "NUMBER OF NODS:", 150+i*50
     # TODO: distruggere l'oggetto "net" in ogni ciclo
-    net = network.Network([2, 350, 3])
+    net = network.Network([4, 350, 200, 3])
     net.SGD(training_data, epochs, mini_batch_size, learning_rate)
     results, n_test, accuracy = net.test_network(test_data)
     print "\n{0} / {1} \nAccuracy: {2}%".format(results, n_test, accuracy)
-    with open(filename, 'a') as log:
-        log.write("%f\n" % (accuracy))
-
+    # with open(filename, 'a') as log:
+    #     log.write("%f\n" % (accuracy))
